340b4444b98443d43218398c3a0aa6343c823e8c
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2022-01-02 19:20:10 -0600

    added tests for manager

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -7,21 +7,36 @@ describe('Intern', () => {
     describe('instantiation', () => {
         it('should pass and instantiate object correctly.', () => {
             // arrange
-            let testIntern = new Intern ("Jason", 1, "jasong@gmail.com", "University of Texas");
+            let testIntern = new Intern ("Jason", 1, "jasong@gmail.com", "Intern", "University of Texas");
             //act
             //assert
-            expect(testIntern.school).toBe("University of Texas")
+            expect(testIntern.name).toEqual("Jason");
+            expect(testIntern.id).toEqual(1);
+            expect(testIntern.email).toEqual("jasong@gmail.com");
+            expect(testIntern.role).toEqual("Intern");
+            expect(testIntern.school).toEqual("University of Texas");
         });
     });
 
     describe('getSchool', () => {
         it('should return school name correctly.', () => {
             // arrange
-            let testIntern = new Intern ("Jason", 1, "jasong@gmail.com", "University of Texas");
+            let testIntern = new Intern ("Jason", 1, "jasong@gmail.com", "Intern", "University of Texas");
             //act
             let school = testIntern.getSchool();
             //assert
-            expect(school).toBe("University of Texas");
+            expect(school).toEqual("University of Texas");
+        });
+    });
+    
+    describe('getRole', () => {
+        it('should return school name correctly.', () => {
+            // arrange
+            let testIntern = new Intern ("Jason", 1, "jasong@gmail.com", "Intern", "University of Texas");
+            //act
+            let role = testIntern.getRole();
+            //assert
+            expect(role).toEqual("Intern");
         });
     });
 
